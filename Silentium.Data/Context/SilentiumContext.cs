9a43314e2dc60cce6e31af71a6f7112ceffe360d
Criss Guinther <crissguinther@proton.me>
2023-01-29 12:23:40 -0300

    Selenium feat: MySql Selenium DbContext

---
+++ Silentium.Data/Context/SilentiumContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Silentium.Data.Context {
+    public class SilentiumContext : DbContext {
+        public SilentiumContext(DbContextOptions options) : base(options) {         
+        }
+    }
+}
---
+++ Silentium/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silentium.WebAPI.Controllers {
+    public class UserController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
